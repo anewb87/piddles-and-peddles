9190743bc562d07f1cc77c8bef20b5c39b83272d
Alexis Newby <alexis.newby@gmail.com>
2022-03-03 20:11:42 -0700

    Add bike png and style links

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,19 +5,26 @@ import bryce from '../../Assets/bryce.jpg';
 import canyon from '../../Assets/canyon.jpg';
 import capitol from '../../Assets/capitol.jpg';
 import zion from '../../Assets/zion.jpg';
+import bike from '../../Assets/bike.png'
 import './Home.scss';
 
 const Home = () => {
 
+    const linkStyle = {
+        // textAlign: 'center',
+        textDecoration: 'underline',
+    }
+
     return (
         <section className ='home-page'>
-            <Link to='toilets'>Toilet Talk</Link>
             <section className='mighty-five'>
-                <div>
+                <div className='intro-box'>
                     <h1 className='mighty-title'>THE MIGHTY FIVE</h1>
-                    <h2 className='toilet-title'>& their toilets</h2>
+                    <Link className='toilet-title' style={linkStyle} to='toilets'>& their toilets</Link>
+                    {/* <h2 className='toilet-title'>& their toilets</h2> */}
                     <h2 className='mighty-info'>Yep. You read that right. Not only is southern Utah home to 'The Mighty Five' National Parks, but it's also home to their toilets! With hundres of miles of roads to ramble down, cyclists can now enjoy Mother Nature without having to worry about her inconvenient calls. So grab your helmet, clip-in, and chase that dreamland deuce. You deserve a poo with a view! We're #1 for your #2.
                     </h2>
+                    <img className='bike' src={bike} alt='bike logo with toilet paper back wheel'/>
                 </div>
                 <Link to='arch/park'>
                     <img className='park-images' src={arches} alt='Picture of Arches National Park that is a link to take you to Arches Main Page'/>
