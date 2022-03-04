d1797739035c3b140723d895bb417fce8bb1a077
Alexis Newby <alexis.newby@gmail.com>
2022-03-04 00:22:28 -0700

    Get button to add bathroom to state

--- src/Components/ToiletCard/ToiletCard.js
+++ src/Components/ToiletCard/ToiletCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './ToiletCard.scss';
 
-const ToiletCard = ({ id, location, region, type }) => {
+const ToiletCard = ({ id, location, region, type, isSafe, addToSafe, addToUnsafe }) => {
+
+    console.log(isSafe)
+
     return (
-        <div className='card'>
+        <section className='card'>
             <h2>{location}</h2>
             <p>{region}</p>
             <p>type: {type}</p>
-        </div>
+            {!isSafe.includes({ id, location, region, type }) ? 
+                <button onClick={() => addToSafe({ id, location, region, type })}>YES</button> :
+                <button onClick={() => addToSafe({ id, location, region, type })}>NO</button>}
+            {/* <button>NO</button> */}
+        </section>
     )
 }
 
