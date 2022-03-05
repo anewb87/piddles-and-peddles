a86e0e2ca98e18b7c2462b25aac43730d215ecb8
Alexis Newby <alexis.newby@gmail.com>
2022-03-04 19:39:40 -0700

    Reformat Park component

--- src/App.js
+++ src/App.js
@@ -4,30 +4,34 @@ import Home from './Components/Home/Home';
 import ToiletTypes from './Components/ToiletTypes/ToiletTypes'
 import Park from './Components/Park/Park';
 import ParkToilets from './Components/ParkToilets/ParkToilets';
-import ParkFacts from './Components/ParkFacts/ParkFacts';
+// import ParkFacts from './Components/ParkFacts/ParkFacts';
 import Error from './Components/Error/Error';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      selectedParkCode: '',
+      // selectedParkCode: '',
       currentPark: {},
       parkToilets: [],
       error: '',
     }
   }
 
+  getInfoFromPark = (parkData) => {
+    this.setState({ currentPark: parkData })
+  }
+
   render() {
     return (
-      <main className = 'app'>
+      <main className= 'app'>
         <Switch>
-          <Route exact path='/' render={() => <Home getInfo={this.implementParkInfo}/>} />
-          <Route exact path='/toilets' render={() => <ToiletTypes/>} />
+          <Route exact path='/' render={() => <Home implementInfo={this.implementParkInfo} />} />
+          <Route exact path='/toilets' render={() => <ToiletTypes />} />
           <Route exact path='/:parkroute/park' render={({ match }) => {
-            return <Park parkName={match.params.parkroute} />
+            return <Park parkName={match.params.parkroute} getInfo={this.getInfoFromPark} currentPark={this.state.currentPark} />
           }}/>
-          <Route exact path='/:parkroute/park/info' render={({ match }) => <ParkFacts parkName={match.params.parkroute} />} />
+          {/* <Route exact path='/:parkroute/park/info' render={({ match }) => <ParkFacts parkName={match.params.parkroute} />} /> */}
           <Route exact path='/:parkroute/park/potties' render={( { match } ) => <ParkToilets parkName={match.params.parkroute}/>} />
           <Route exact path='/error' render={() => <Error/>} />
           <Redirect to='/error'/>
