5e1720a89c736e2060271dc4091e9799f63b01b1
Alexis Newby <alexis.newby@gmail.com>
2022-03-06 13:34:26 -0700

    Clean up, delete commented out code

--- src/Components/ToiletCard/ToiletCard.js
+++ src/Components/ToiletCard/ToiletCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ToiletCard.scss';
 
-const ToiletCard = ({ id, park, location, region, type, toilet, addToSafe, removeFromSafe, postToilet, deleteToilet, checkedStatus }) => {
+const ToiletCard = ({ id, park, location, region, type, toilet, postToilet, deleteToilet, checkedStatus }) => {
 
     const handleCheckbox = (event) => {
         if(event.target.checked) {
@@ -19,7 +19,6 @@ const ToiletCard = ({ id, park, location, region, type, toilet, addToSafe, remov
             <h3>{location}</h3>
             <p>{region}</p>
             <p>Toilet Type: {type}</p>
-            {/* <p>My Bike Feels Safe Here:</p> */}
             <label htmlFor='safetycheck'>My Bike Feels Safe Here:</label><br/>
             <input id='safetycheck' onChange={(event) => handleCheckbox(event)} type='checkbox' checked={checkedStatus} />
         </section>
@@ -35,48 +34,7 @@ ToiletCard.propTypes = {
     region: PropTypes.string,
     type: PropTypes.string,
     toilet: PropTypes.object,
-    addToSafe: PropTypes.func,
-    removeFromSafe: PropTypes.func,
     postToilet: PropTypes.func,
     deleteToilet: PropTypes.func,
+    checkedStatus: PropTypes.bool
 }
-
-
-//old stuff below 6:43
-
-// import React from 'react';
-// import Toggle from '../Toggle/Toggle';
-// import PropTypes from 'prop-types';
-// import './ToiletCard.scss';
-
-// const ToiletCard = ({ id, location, region, type, isSafe, isToggled, onToggle, addToSafe, toilet, post, handleChange, addToUnsafe }) => {
-
-//     return (
-//         <section className='card'>
-//             <h2>{location}</h2>
-//             <p>{region}</p>
-//             <p>type: {type}</p>
-//             {/* <button onClick={() => addToSafe(toilet)}>safe?</button> */}
-
-
-//             <Toggle id={id} post={post} isToggled={isToggled} />
-//             {/* <button onClick={() => post(toilet)}>safe?</button> */}
-
-
-//             {/* {!isSafe.includes({ id, location, region, type }) ? 
-//                 <button onClick={() => addToSafe({ id, location, region, type })}>YES</button> :
-//                 <button onClick={() => addToSafe({ id, location, region, type })}>NO</button>} */}
-
-//         </section>
-//     )
-// }
-
-// export default ToiletCard
-
-// ToiletCard.propTypes = {
-//     location: PropTypes.string,
-//     region: PropTypes.string,
-//     type: PropTypes.string,
-//     isToggled: PropTypes.bool,
-//     onToggle: PropTypes.func
-// }
--- src/Components/ToiletSafetyReviews/ToiletSafetyReviews.js
+++ src/Components/ToiletSafetyReviews/ToiletSafetyReviews.js
@@ -25,25 +25,21 @@ class ToiletSafetyReviews extends Component {
     }
 
     postAToilet = (newToilet) => {
-        console.log('post line 74', this.state)
-
         if (!this.state.postedToilets.includes(newToilet)) {
             postToilet(newToilet)
                 .catch(error => this.setState({ error: error }))
         }
     }
 
-
     deleteAToilet = (toiletId) => {
         deleteRatedToilet(toiletId)
-            // .then(reponse => getToiletPostings())
             .then(response => {
                 const removedToilet = this.state.postedToilets.find(toilet => toilet.id === toiletId);
                 const filteredToilets = this.state.postedToilets.filter(toilet => toilet.id !== removedToilet.id)
 
                 this.setState({ postedToilets: filteredToilets })
             })
-            
+
             .catch(error => this.setState({ error: error })) 
     }
 
@@ -101,78 +97,3 @@ class ToiletSafetyReviews extends Component {
 }
 
 export default ToiletSafetyReviews
-
-
-// createSafeToiletCards = () => {
-//     const safeCards = this.state.reviewedToilets.map(toilet => {
-//         return (
-//             <ToiletCard
-//                 key={toilet.id}
-//                 id={toilet.id}
-//                 location={toilet.location}
-//                 // region={toilet.region}
-//                 type={toilet.type}
-//             // addToSafe={this.addToSafe}
-//             // addToUnsafe={this.addToUnsafe}
-//             // isSafe={this.state.isSafe}
-//             // toilet={toilet}
-//             />
-//         )
-//     })
-//     return safeCards
-// }
-
-
-
-// const ToiletSafetyReviews = ({ safeToilets }) => {
-
-//     const determineDisplay = () => {
-//         if (safeToilets.length === 0) {
-//             return <section>
-//                 How comfortable do you feel leaving your bike while you do your dooties? Pick your park, visit a privy, and rate how safe you feel your bike was at this location.
-//                 <Link to='/'>Show Me the Mighty Five</Link>
-//             </section>
-//         } else {
-//             return createToiletCards()
-//         }
-//     }
-
-//     const createToiletCards = () => {
-//         const toiletCards = safeToilets.map(toilet => {
-//             return (
-//                 <ToiletCard
-//                     key={toilet.id}
-//                     id={toilet.id}
-//                     location={toilet.location}
-//                     region={toilet.region}
-//                     type={toilet.type}
-//                     isToggled={this.state.isToggled}
-//                     onToggle={this.onToggle}
-//                 // isSafe={this.state.isSafe}
-//                     // toilet={toilet}
-//                 />
-//             )
-//         })
-
-//         return toiletCards
-//     }
-
-//     return (
-//         <section>
-//             {this.determineDisplay()}
-//         </section>
-//     )
-
-//     // render() {
-//     //     return (
-//     //         <section>
-//     //             {this.determineDisplay()}
-//     //         </section>
-//     //     )
-//     // }
-// }
-
-// export default ToiletSafetyReviews
-
-
-
