0dfd8293615a3ad59adaabdd88c7ebd71bf096ed
Alexis Newby <alexis.newby@gmail.com>
2022-03-18 15:11:01 -0600

    Beautify type types component

--- src/Components/ToiletTypes/ToiletTypes.js
+++ src/Components/ToiletTypes/ToiletTypes.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
 import './ToiletTypes.scss'
 
 const ToiletTypes = () => {
     return (
-        <section>
-            <Link to='/'>
+        <section className='toilet-types-page'>
+            {/* <Link to='/'>
                 <button>Home</button>
-            </Link>
-            <h2>DEAREST INSTRUCTORS, IN THE FUTURE THIS WILL BE THE HIGH HOLY PLACE TO FOUND OUT INFORMATION ABOUT THE THREE TOILET TYPES OF UTAH'S NATIONAL PARKS. SINCE IT'S SIMPLY SHOWING DATA AND DOESN'T HAVE ANYTHING TO DO WITH MVP, I THOUGHT/HOPED IT'D BE OK TO LEAVE HERE (UNTESTED) SO I CAN STILL HAVE IT AT THE READY RATHER THAN DELETING IT. THANKS MUCH. </h2>
+            </Link> */}
+            <Header/>
+            <h2 className='info'>DEAREST READER, IN THE FUTURE THIS WILL BE THE HIGH HOLY PLACE TO FIND OUT INFORMATION ABOUT THE THREE TOILET TYPES OF UTAH'S NATIONAL PARKS. </h2>
         </section>
     )
 }
